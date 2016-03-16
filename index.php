9051b6445338e85b3c951773d45f8ef4e046dfb3
asif <asifadam93@hotmail.fr>
2016-03-16 16:20:03 +0100

    premier test asif

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Accueil || MIRAMAP</title>
+    <title>Accueil || MIRAMP</title>
 	<!-- All css Files Here -->
 	<!-- fonts -->
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,500' rel='stylesheet' type='text/css'>
