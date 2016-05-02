2141aeafba312f4d264f1a8f5fcf0e150d46aa3e
asif <asifadam93@hotmail.fr>
2016-05-02 15:16:39 +0200

    contact.php modifier

--- contact.php
+++ contact.php
@@ -83,9 +83,9 @@
                         <ul>
                             <li><a href="index.php">Accueil</a></li>
                             <li><a href="#">A propos</a></li>
-                            <li><a href="#">Les Produits</i></a></li>
-                            <li><a href="#">Les Magasins</a></li>
-                            <li><a href="#">CONTACT</a></li>
+                            <li><a href="produits.php">Les Produits</i></a></li>
+                            <li><a href="magasins.php">Les Magasins</a></li>
+                            <li><a href="contact.php">CONTACT</a></li>
                             <li class="shop_icon">
                                 <a href="#"><img src="images/menu_icon_img.png" alt=""/></a>
                                 <span>10</span>
@@ -117,7 +117,70 @@
 <!-- Header-Section-End  -->
 
 
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+
+<div class="contact_us section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="contact_us">
+                    <h4>Envoyer un message</h4>
+                    <form class="form-horizontal" id="mess_form">
+                        <div class="form-group">
+                            <div class="col-sm-6">
+                                <input type="text" class="form-control" id="inputName" placeholder="Nom">
+                            </div>
+                            <div class="col-sm-6">
+                                <input type="email" class="form-control" id="inputEmail3" placeholder="@">
+                            </div>
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <div class="col-sm-6">
+                                <input type="email" class="form-control" id="inputEmail2" placeholder="Tel">
+                            </div>
+                            <div class="col-sm-6">
+                                <input type="text" class="form-control" id="inputSubject" placeholder="Objet">
+                            </div>
+                        </div>
+                        <br>
+                        <div class="form-group">
+                            <div class="col-sm-12">
+                                <textarea class="form-control" placeholder="Message" rows="9"></textarea>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-12 text-center">
+                                <button type="submit" class=" shop_now_btn btn btn-default hvr-bounce-to-left">Envoyer</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="address_text">
+                    <div class="single_address">
+                        <br><br><p><span>Tel</span></p>
+                        <p>04 81 91 60 51</p>
+                    </div>
+                    <div class="single_address">
+                        <p><span>Email</span></p>
+                        <p>contact@miramap.org</p>
+                    </div>
+                    <div class="single_address">
+                        <p><span>Address:</span></p>
+                        <p>MIRAMAP</p>
+                        <p>58 rue Rauli,</p>
+                        <p>69007 Lyon</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
 
 
 <!-- Footer-Section-Start -->
