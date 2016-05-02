12aeab804b4db14d0ff7e7830a7b95eaa75c57cf
asif <asifadam93@hotmail.fr>
2016-05-02 13:58:42 +0200

    Maps modification

--- contactBis.php
+++ contactBis.php
@@ -22,15 +22,11 @@
     <link rel="stylesheet" href="css/responsive.css"/>
     <!-- favicon -->
     <link rel="shortcut icon" href="images/favicon.png"/>
-
-    <script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
-
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-
     <![endif]-->
 </head>
 <body>
@@ -118,7 +114,11 @@
 <!-- Header-Section-End  -->
 
 
-<div id="map"></div>
+<!-- Google-Map-Section-start -->
+<div class="container">
+    <div id="googleMap"></div>
+</div>
+<!-- Google-Map-Section-end -->
 
 
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
@@ -209,42 +209,6 @@
     </div>
 </footer>
 <!-- Footer-Section-End -->
-
-
-<script type="text/javascript">
-    var locations = [
-        ['Amap IDF', 48.8615144, 2.3518197, 5],
-        ['Amap Lille', 50.292134, 2.792142, 5],
-        ['Amap Châteauroux', 46.817514, 1.7030512, 5],
-        ['Miramap Annecy', 45.9154129, 6.1328845, 5]
-    ];
-
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 6,
-        center: new google.maps.LatLng(46.7, 2.5),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-
-    var infowindow = new google.maps.InfoWindow();
-
-    var marker, i;
-
-    for (i = 0; i < locations.length; i++) {
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-            map: map
-        });
-
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-                infowindow.setContent(locations[i][0]);
-                infowindow.open(map, marker);
-            }
-        })(marker, i));
-    }
-</script>
-
-
 <!-- All js Files Here -->
 <!-- jquery-1.11.3 -->
 <script src="js/jquery-1.11.3.min.js"></script>
@@ -260,7 +224,26 @@
 <script src="js/jquery.scrollUp.js"></script>
 <!-- mobilemenu js -->
 <script src="js/jquery.meanmenu.js"></script>
-
+<!-- Google Map js -->
+<script src="https://maps.googleapis.com/maps/api/js"></script>
+<script>
+    function initialize() {
+        var mapOptions = {
+            zoom: 15,
+            scrollwheel: false,
+            center: new google.maps.LatLng(45.746369, 4.833793)
+        };
+        var map = new google.maps.Map(document.getElementById('googleMap'),
+            mapOptions);
+        var marker = new google.maps.Marker({
+            position: map.getCenter(),
+            animation: google.maps.Animation.BOUNCE,
+            icon: 'images/map-marker.png',
+            map: map
+        });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+</script>
 <!-- main js -->
 <script src="js/main.js"></script>
 </body>
