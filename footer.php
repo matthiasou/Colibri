c6a495e5feadb2547da359a22bb2d41989dca5f5
matthiasou <matthiasou@yahoo.fr>
2016-05-24 21:28:23 +0200

    amélioration du code avec header.php et footer.php

---
+++ footer.php
@@ -0,0 +1,104 @@
+
+<!-- Footer-Section-Start -->
+<footer>
+    <div class="footer_middel footer-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-md-3">
+                    <div class="footer_link address">
+                        <p>MIRAMAP</p>
+                        <ul>
+                            <li><span>Adresse: </span> 58 rue Raulin - 69007 Lyon</li>
+                            <li><span>Telephone: </span>04 81 91 60 51</li>
+                            <li><span>Email: </span>contact@miramap.fr</li>
+                        </ul>
+                    </div>
+                    <div class="footer_icon">
+                        <ul>
+                            <li><a href="https://www.facebook.com/profile.php?id=100010052358058&fref=ts"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="footer_link">
+                        <p>Compte </p>
+                        <ul>
+                            <li><a href="#">Mon compte</a></li>
+                            <li><a href="cart.php">Panier</a></li>
+                            <li><a href="#">Achats</a></li>
+                            <li><a href="#">Code promo</a></li>
+                            <li><a href="#">Informations</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="footer_link res_mar">
+                        <p>Support</p>
+                        <ul>
+                            <li><a href="#">Test</a></li>
+                            <li><a href="#">Test</a></li>
+                            <li><a href="#">Test</a></li>
+                            <li><a href="#">Test</a></li>
+                            <li><a href="#">Test</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="footer_link res_mar">
+                        <p>information</p>
+                        <ul>
+                            <li><a href="#">Test</a></li>
+                            <li><a href="#">Test</a></li>
+                            <li><a href="#">Test</a></li>
+                            <li><a href="#">Test</a></li>
+                            <li><a href="#">Test</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer_bottom footer-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-xs-12">
+                    <div class="copyright">
+                        <p>Copyright 2015 <a href="http://designscrazed.org/">Miramap</a> All rights reserved</p>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-xs-12">
+                    <div class="copyright_icon text-right">
+                        <a href="#"><img src="images/footer/paypal-1.png" alt="" /></a>
+                        <a href="#"><img src="images/footer/paypal-2.png" alt="" /></a>
+                        <a href="#"><img src="images/footer/paypal-3.png" alt="" /></a>
+                        <a href="#"><img src="images/footer/paypal-4.png" alt="" /></a>
+                        <a href="#"><img src="images/footer/paypal-5.png" alt="" /></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer-Section-End -->
+<!-- All js Files Here -->
+<!-- jquery-1.11.3 -->
+<script src="js/jquery-1.11.3.min.js"></script>
+<!-- bootstrap js -->
+<script src="js/bootstrap.min.js"></script>
+<!-- revolution js -->
+<script type="text/javascript" src="lib/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+<script type="text/javascript" src="lib/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+<script src="lib/rs-plugin/rs.home.js"></script>
+<!-- search-box js -->
+<script src="js/search-box.js"></script>
+<!-- scrollUp js -->
+<script src="js/jquery.scrollUp.js"></script>
+<!-- mobilemenu js -->
+<script src="js/jquery.meanmenu.js"></script>
+<!-- main js -->
+<script src="js/main.js"></script>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,5 @@
+</div>
+
+<?php include("header.php"); ?>
+<?php include("footer.php"); ?>
+<div>
