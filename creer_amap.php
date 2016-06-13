dc87fb2c2c061ac9125b709ebf908c1c4c347d98
asif <asifadam93@hotmail.fr>
2016-06-13 14:20:41 +0200

    texte ajouter : creer amap

--- creer_amap.php
+++ creer_amap.php
@@ -1,20 +1,37 @@
-<?php include("header.php");
-/**
- * Created by PhpStorm.
- * User: matthiaslecomte
- * Date: 05/06/2016
- * Time: 15:56
- */
-
-?>
-    <br xmlns="http://www.w3.org/1999/html"><br>
+<?php include("header.php"); ?>
 
     <div class="container">
-        <h5>Créer une amap ? </h5>
-        <p href="http://www.reseau-amap.org/creer-AMAP.php" style="color: #0f3e68">http://www.reseau-amap.org/creer-AMAP.php</p>
 
+        <br><h5>Créer une amap ? </h5>
 
+        <p id="textapropos"><b>* Constituer un groupe pour démarrer le projet.</b>
+            Les AMAP tournent autour de 60 adhérents. Mais on peut commencer à 6 !
+            En fait, tout dépend de la volonté de l’agriculteur et de la taille de son exploitation.
+            En fonction du nombre d'adhérents, il continuera ou non en parallèle ses autres activités (marchés, grandes surfaces, etc.).
+            L'augmentation peut être progressive. Mais pas d'inquiétude, la plupart des AMAP ont une liste d'attente d'adhérents !
+        <p><br>
+
+        <p id="textapropos"><b>* Formaliser le projet.</b> Cela se fait avec l'aide du réseau régional des AMAP, CREAMAP
+            ou à défaut avec Alliance Provence, qui vous aidera dans les étapes suivantes.
+        <p><br>
+
+        <p id="textapropos"><b>* Recruter des adhérents.</b> Cela peut se faire via les amis et voisins,
+            en mettant une annonce à la mairie, à l'école, dans la presse locale, mais aussi via le réseau AMAP,
+            les réseaux associatifs locaux, etc.
+        <p><br>
+
+        <p id="textapropos"><b>* Rechercher un producteur local.</b> Le réseau, mais aussi les filières biologique et paysanne,
+            peuvent vous aider. Il doit pratiquer une agriculture écologique proche du bio ou ne pas être réticent à s'y
+            mettre progressivement (un délai lui sera octroyé). Parfois, il sera nécessaire de trouver des terres à proximité
+            de votre commune, si besoin avec l'aide de la commune, qui, rappelons-le, détermine les zones agricoles de son territoire,
+            ou avec l'aide d'associations (comme Terre de Liens).
+        <p><br>
+
+        <p id="textapropos"><b>* Constituer une association loi 1901.</b> Cette structure juridique est nécessaire,
+            car plusieurs contrats vont être signés : un contrat entre l'AMAP et l'agriculteur, qui définit votre relation,
+            les produits, leur nature, leur prix, les livraisons, etc.; et un contrat entre l'agriculteur et chaque adhérent,
+            afin de définir la période d'engagement, le mode de paiement, etc.
+        <p><br>
     </div>
-
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <br>
 <?php include("footer.php"); ?>
